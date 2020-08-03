8ce58b1b867340771248c24f5486eb7c7db6c387
Vipin Bhuvanachandran <b.vipin@outlook.com>
2020-08-03 19:21:03 +0530

    port issues

--- index.js
+++ index.js
@@ -47,8 +47,11 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 function pushToGame(createData)
